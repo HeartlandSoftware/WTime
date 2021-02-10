e4a54a5a38e167ffb0507a64fa77fea3327dc5b0
Travis Redpath <travis_redpath@live.ca>
2021-02-10 15:38:12 -0600

    Update the copyright year
    Change the copyright year from 2019 to 2021.

--- cxx/include/internal/worldlocation.h
+++ cxx/include/internal/worldlocation.h
@@ -1,7 +1,7 @@
 /**
  * worldlocation.h
  *
- * Copyright 2016-2019 Heartland Software Solutions Inc.
+ * Copyright 2016-2021 Heartland Software Solutions Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
